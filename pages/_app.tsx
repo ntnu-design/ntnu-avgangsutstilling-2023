7764867b7728e36b2ac49b13db31ef04cfde3b1e
Misthalin <72019688+Misthalin@users.noreply.github.com>
2023-02-22 15:06:26 +0100

    predefined weights according to designmanual

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import "../styles/index.css";
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-primary",
+  style: ["normal"],
+  weight: ["400", "500", "600", "700"],
+  fallback: ["system-ui", "arial"],
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {

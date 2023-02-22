b3be84ca060ae548479c77fc6e9072dbcd6def64
Misthalin <72019688+Misthalin@users.noreply.github.com>
2023-02-22 14:28:13 +0100

    installed montserrat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import { Montserrat } from "@next/font/google";
+import "../styles/index.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }

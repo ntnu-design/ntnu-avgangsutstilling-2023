4b227fe88e03ce6a67d2c3842deeb883be60588c
monsst <monsst@stud.ntnu.no>
2023-03-15 18:17:34 +0100

    added 2 components, some icons for homepage, added custom screen size in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,10 @@ module.exports = {
       boxShadow: {
         sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
         md: "0 8px 30px rgba(0, 0, 0, 0.12)",
-      },
+      },    
+      screens: {
+      'smd': { 'min': '768px', 'max': '1536px' }
+    }
     },
   },
   plugins: [],

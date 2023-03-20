22fb7e7e96260a00a05857fd513e048f0c8b4c8e
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-03-20 10:01:40 +0100

    upgraded to next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import { Montserrat } from "@next/font/google";
+import { Montserrat } from "next/font/google";
 import "../styles/index.css";
 
 const montserrat = Montserrat({

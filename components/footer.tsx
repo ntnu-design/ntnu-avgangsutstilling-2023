d5e582ab63542b7874da8f73c2d6591a3c94d603
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-03-20 10:16:51 +0100

    fixed bg color footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,5 @@
 import Container from "./layout/container"
-import { IconContext } from "react-icons"
-import { FaFacebook } from "react-icons/fa"
-import { BsInstagram, BsTwitter, BsLinkedin } from "react-icons/bs"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
 
 function Footer() {
     let mainBg = "bg-[#F47A43]"
@@ -12,42 +8,35 @@ function Footer() {
     let bmedBg = "bg-[#D95C95]"
 
     const router = useRouter()
+    console.log(router);
+    
 
     return (
         <footer
             className={`flex justify-center ${
-                router.asPath == "/bixd" ? bixdBg : 
-                router.asPath == "/bwu" ? bwuBg : 
-                router.asPath == "/bmed" ? bmedBg : mainBg
+                router.query.studyProgramme == "bixd"
+                    ? bixdBg
+                    : router.query.studyProgramme == "bwu"
+                    ? bwuBg
+                    : router.query.studyProgramme == "bmed"
+                    ? bmedBg
+                    : mainBg
             }`}
         >
             <Container>
-                <IconContext.Provider value={{ color: "white" }}>
-                    <div className="py-20 flex flex-col items-center justify-center">
-                        <h1 className="md:text-lg sm:text-md text-white">
-                            Følg oss på våre sosiale medier!
-                        </h1>
-                        <div className="flex justify-center text-lg gap-7 pt-5">
-                            <a href="https://" target="_blank">
-                                <FaFacebook size={30} />
-                            </a>
-                            <a
-                                href="https://www.instagram.com/avgangsutstilling_ntnu/"
-                                target="_blank"
-                            >
-                                <BsInstagram size={30} />
-                            </a>
-                            <a href="http://" target="_blank">
-                                <BsTwitter size={30} />
-                            </a>
-                            <a href="http://" target="_blank">
-                                <BsLinkedin size={30} />
-                            </a>
-                        </div>
-                        <div></div>
+                <div className="py-20 flex flex-col items-center justify-center text-white">
+                    <h1 className="md:text-lg sm:text-md">
+                        Følg oss på våre sosiale medier!
+                    </h1>
+                    <div className="flex justify-center text-lg gap-7 pt-5">
+                        <a
+                            href="https://www.instagram.com/avgangsutstilling_ntnu/"
+                            target="_blank"
+                        >
+                            <img src="/instagramicon.svg" alt="instagram" />
+                        </a>
                     </div>
-                </IconContext.Provider>
-                ;
+                </div>
             </Container>
         </footer>
     )
